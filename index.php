83348a359fbb5df55e09083113758d2680017b1a
Antonio Duprez <antonioduprezher@gmail.com>
2018-04-10 18:48:17 +0200

    Aviso legal (inicio) hecho

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
-<?php
-    echo "hola";
-?>
 <html>
     <head>
+        <!-- metas -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
         <!-- CSS Styling Link -->
         <link 
             rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
             integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
             crossorigin="anonymous">
+        <link href="css/style.css" rel="stylesheet">
 
         <!-- Scripting link (Jquery, Angular, bootstrap script, etc) -->
         <script
@@ -32,9 +35,78 @@
 
     </head>
     <body>
-        <div class="text-center">
-            <h1>¡Hello world!</h1>
-            <button class="btn btn-primary">Button bootstrap ready</button>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs">
+                    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+                        <div class="container">
+                            <a class="navbar-brand js-scroll-trigger" href="#page-top">Clinican</a>
+                            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarResponsive">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item">
+                                <a class="nav-link js-scroll-trigger" href="#about">Gestionar perros</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link js-scroll-trigger" href="#services">Nuevo perro</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link js-scroll-trigger" href="#portfolio">Historial</a>
+                                </li>
+                                <li>
+                                <a class="nav-link">Configuración</a>
+                                <i class="fas fa-cog"></i>
+                                </li>
+                            </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            <div class="cuerpo">
+                <div class="row">
+                    <div class="col-12 col-md-10 offset-md-1 col-xl-4 offset-xl-1">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate non risus nec condimentum. Quisque efficitur arcu eget ornare fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla lobortis id ligula a dapibus. Sed tincidunt, lorem non molestie vestibulum, mi ante bibendum magna, eget lobortis ex velit in nisl. Mauris ac dolor ut nulla interdum malesuada eget et velit. Duis eu lorem eleifend, tincidunt dui non, rhoncus risus. Nullam semper ultrices mattis. Sed vel semper tellus. Pellentesque ut ex vitae tortor faucibus tincidunt. Suspendisse fermentum, diam a tempus auctor, neque leo tincidunt orci, a ullamcorper lorem elit nec risus.
+                    Proin nunc augue, lobortis vel aliquam ut, porttitor in sapien. Curabitur eu orci nisi. Pellentesque neque nibh, hendrerit egestas orci eu, porta malesuada elit. Vestibulum magna nisl, viverra non orci id, rutrum ullamcorper nunc. Aenean semper vitae nisi ac pellentesque. Nam et diam quis urna faucibus egestas. Vestibulum a sem vel purus volutpat mollis sagittis nec mi. Pellentesque aliquam, ipsum a laoreet tempor, felis mauris sollicitudin metus, non venenatis augue nibh at sapien. Pellentesque hendrerit sapien a tristique hendrerit. Nam viverra metus ante, lobortis vehicula justo posuere vitae. Mauris non luctus nunc, id tempus enim. Maecenas non lectus dui.
+                    </div>
+                    <div class="col-12 col-md-10 offset-md-1 col-xl-4">
+                        <img src="images/avisoLegal1.jpg" alt="Aviso legal" class="img-thumbnail mt-4">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-10 offset-md-1 col-xl-4 offset-xl-2">
+                        <img src="images/avisoLegal2.jpg" alt="Aviso legal" class="img-thumbnail mt-4">
+                    </div>
+                    <div class="col-12 col-md-10 offset-md-1 col-xl-4">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vulputate non risus nec condimentum. Quisque efficitur arcu eget ornare fermentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla lobortis id ligula a dapibus. Sed tincidunt, lorem non molestie vestibulum, mi ante bibendum magna, eget lobortis ex velit in nisl. Mauris ac dolor ut nulla interdum malesuada eget et velit. Duis eu lorem eleifend, tincidunt dui non, rhoncus risus. Nullam semper ultrices mattis. Sed vel semper tellus. Pellentesque ut ex vitae tortor faucibus tincidunt. Suspendisse fermentum, diam a tempus auctor, neque leo tincidunt orci, a ullamcorper lorem elit nec risus.
+                    Proin nunc augue, lobortis vel aliquam ut, porttitor in sapien. Curabitur eu orci nisi. Pellentesque neque nibh, hendrerit egestas orci eu, porta malesuada elit. Vestibulum magna nisl, viverra non orci id, rutrum ullamcorper nunc. Aenean semper vitae nisi ac pellentesque. Nam et diam quis urna faucibus egestas. Vestibulum a sem vel purus volutpat mollis sagittis nec mi. Pellentesque aliquam, ipsum a laoreet tempor, felis mauris sollicitudin metus, non venenatis augue nibh at sapien. Pellentesque hendrerit sapien a tristique hendrerit. Nam viverra metus ante, lobortis vehicula justo posuere vitae. Mauris non luctus nunc, id tempus enim. Maecenas non lectus dui.
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col mt-5 pie">
+                        <footer id="contact">
+                            <div class="container">
+                                <div class="row">
+                                <div class="col-lg-4 ml-auto text-center">
+                                    <i class="fa fa-phone fa-3x mb-3 sr-contact"></i>
+                                    <p>622 64 01 46</p>
+                                </div>
+                                <div class="col-lg-4 mr-auto text-center">
+                                    <a href=""><i class="fab fa-twitter fa-3x mb-3 sr-contact"></i></a>
+                                    <a href=""><i class="fab fa-youtube fa-3x mb-3 sr-contact"></i></a>
+                                    <a href=""><i class="fab fa-facebook fa-3x mb-3 sr-contact"></i></a>
+                                    <p>
+                                    <a href="">Aviso legal</a>
+                                    </p>
+                                </div>
+                                </div>
+                            </div>
+                        </footer>
+                    </div>
+                </div>
+            </div>
         </div>
     </body>
 </html>
