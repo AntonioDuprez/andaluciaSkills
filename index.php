190a89ac20e94d655d8ac37bb734d330aebb7d09
Antonio Duprez <antonioduprezher@gmail.com>
2018-04-10 03:51:58 +0200

    Font awesome linked

--- index.php
+++ index.php
@@ -1,2 +1,40 @@
 <?php
     echo "hola";
+?>
+<html>
+    <head>
+        <!-- CSS Styling Link -->
+        <link 
+            rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
+            crossorigin="anonymous">
+
+        <!-- Scripting link (Jquery, Angular, bootstrap script, etc) -->
+        <script
+          src="https://code.jquery.com/jquery-3.3.1.slim.js"
+          integrity="sha256-fNXJFIlca05BIO2Y5zh1xrShK3ME+/lYZ0j+ChxX2DA="
+          crossorigin="anonymous">//This is a jquery script
+        </script>
+        <script 
+            src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" 
+            crossorigin="anonymous"></script>
+        <script 
+            src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" 
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" 
+            crossorigin="anonymous"></script>
+        <!-- Font Awesome Script-->
+        <script 
+            defer 
+            src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" 
+            integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+
+
+    </head>
+    <body>
+        <div class="text-center">
+            <h1>¡Hello world!</h1>
+            <button class="btn btn-primary">Button bootstrap ready</button>
+        </div>
+    </body>
+</html>
